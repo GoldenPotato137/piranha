f7bc2e3bd0d1e52e91abf152c76c821d702b72b8
goldenpotato137 <goldenpotato137@gmail.com>
2025-08-12 19:53:31 +0800

    vgg16

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./piranha -p 0 -c /home/ubuntu/TDSC-2025/piranha/configs/lenet_mnist_3split.json -o output/P0 -P 0
+./piranha -p 0 -c /home/ubuntu/TDSC-2025/piranha/configs/vgg_cifar10.json -o output/P0 -P 0
--- run_gpu_monitored.sh
+++ run_gpu_monitored.sh
@@ -68,4 +68,4 @@ MON_PID=$!
 echo "GPU 监控 PID=$MON_PID"
 
 echo "启动 piranha..."
-./piranha -p 0 -c "/home/ubuntu/TDSC-2025/piranha/configs/lenet_mnist_3split.json" -o "$OUT_DIR" -P 0
+./piranha -p 0 -c "/home/ubuntu/TDSC-2025/piranha/configs/vgg_cifar10.json" -o "$OUT_DIR" -P 0
