7ba825936d7639731413198369ff177182e73cd3
goldenpotato137 <goldenpotato137@gmail.com>
2025-08-20 20:39:26 +0800

    final

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./piranha -p 0 -c /home/ubuntu/TDSC-2025/piranha/configs/vgg_imagenet.json -o output/P0 -P 0
+./piranha -p 0 -c /home/ubuntu/TDSC-2025/piranha/configs/all_relu.json -o output/P0 -P 0
